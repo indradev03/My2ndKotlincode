dc74269c2ea3e84361271b48185daee9c466dc14
indradev03 <sarojayer37@gmail.com>
2025-05-04 21:50:44 +0545

    2nd basic code

---
+++ app/src/main/java/com/example/mysecondproject/LoginActivity.kt
@@ -0,0 +1,177 @@
+package com.example.mysecondproject
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+class LoginActivity: ComponentActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            // This is the base (Scaffold)
+            Scaffold { innerPadding->
+                LoginBody(innerPadding)
+            }
+        }
+    }
+
+}
+
+
+@Composable
+fun LoginBody(innerPadding: PaddingValues){
+
+    // State variable
+    // import garda suru ma get ra ani set garne
+
+    var  username by remember { mutableStateOf("") }
+    var  password by remember { mutableStateOf("") }
+    var passwordVisibility by remember { mutableStateOf(false) }
+
+    Column (
+        modifier = Modifier
+            .padding(innerPadding)
+            .fillMaxSize()
+            .background(color = Color.White)
+            .padding(top = 50.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+    {
+        Image(
+            painter = painterResource(R.drawable.loginimage),
+            contentDescription = null,
+            modifier = Modifier.height(250.dp).width(250.dp)
+        )
+
+        OutlinedTextField(
+            // Call back funtion ho yo
+            // kunai pani call back le return garyeko
+            // function variable ma hunxa ra tyo variable ko namm 'it' ho
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 10.dp)
+                .padding(top = 50.dp),
+            shape = RoundedCornerShape(12.dp),
+            keyboardOptions = KeyboardOptions(
+                keyboardType =KeyboardType.Email
+            ),
+
+
+
+            label = {
+                Text("Email")
+            },
+
+            placeholder = {
+                Text("Saroj@gmail.com")
+            },
+            value = username,
+            onValueChange = {input->
+                    username = input
+
+            }
+
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        OutlinedTextField(
+            // Call back funtion ho yo
+            // kunai pani call back le return garyeko
+            // function variable ma hunxa ra tyo variable ko namm 'it' ho
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 10.dp),
+            shape = RoundedCornerShape(12.dp),
+            // Lukaune kaam garxa
+            visualTransformation =
+                if (passwordVisibility) PasswordVisualTransformation()
+                else VisualTransformation.None,
+            // Keyboard Option EMail, Text, Password
+
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Password
+            ),
+
+            prefix = {
+                Icon(
+                    painter = painterResource(R.drawable.baseline_lock_outline_24),
+                    contentDescription = null
+                )
+
+            },
+
+            suffix = {
+                val visibilityIcon = if (passwordVisibility) {
+                    R.drawable.baseline_visibility_24 // 👁️ Visible icon
+                } else {
+                    R.drawable.baseline_visibility_off_24 // 🙈 Hidden icon
+                }
+
+                Icon(
+                    painter = painterResource(id = visibilityIcon),
+                    contentDescription = if (passwordVisibility) "Hide password" else "Show password",
+                    modifier = Modifier.clickable {
+                        passwordVisibility = !passwordVisibility
+                    }
+                )
+            },
+
+//            label = {
+//                Text("Password")
+//            },
+            placeholder = {
+                Text("******")
+            },
+            value = password,
+            onValueChange = {input->
+                password = input
+
+            }
+
+        )
+
+    }
+}
+
+@Preview
+@Composable
+fun LoginPreBody(){
+    LoginBody(innerPadding = PaddingValues(0.dp))
+}
