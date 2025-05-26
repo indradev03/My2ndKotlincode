7aa1f013e8e937f587c231fad4b451c540e0f143
indradev03 <sarojayer37@gmail.com>
2025-05-26 10:32:31 +0545

    horizontal scroll bar like insta stories

--- app/src/main/java/com/example/mysecondproject/ListViewActivity.kt
+++ app/src/main/java/com/example/mysecondproject/ListViewActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
 
@@ -17,9 +19,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 
 import androidx.compose.material3.Scaffold
@@ -28,7 +33,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -51,7 +60,23 @@ fun ListViewBody(innerPadding: PaddingValues){
     val scrollState1 = rememberScrollState()
     val scrollState2 = rememberScrollState()
 
+    // this is for horizontaln scroll bar like insta stories
+    val images = listOf(R.drawable.img1,
+        R.drawable.img2,R.drawable.img3, R.drawable.img4 , R.drawable.img5,
+        R.drawable.img6,R.drawable.img7,R.drawable.img8,R.drawable.img9 )
 
+    val name = listOf(
+        // jun order ma image xa tei order ma name rakhne
+        "Thor1",
+        "Thor2",
+        "Thor3",
+        "Thor4",
+        "Thor5",
+        "Thor6",
+        "Thor7",
+        "Thor8",
+        "Thor("
+    )
 
 //
 //    Column (
@@ -137,77 +162,146 @@ fun ListViewBody(innerPadding: PaddingValues){
 //    }
 
 
-    //lazy coloum just horizental ko laagi matra hunxa
+    //lazy coloum just vertical ko laagi matra hunxa
     LazyColumn (
-        modifier = Modifier.fillMaxSize().background(color = Color.White).padding(50.dp)
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White)
+            .padding(50.dp)
     ) {
         item {
 
+            LazyRow(modifier = Modifier.fillMaxWidth())
+            {
+                items(images.size){ index ->
+                    Image(
+                        painter = painterResource(images[index]),
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.size(100.dp)
+                            .border(
+                                width = 2.dp, color = Color.Blue,
+                                shape = CircleShape
+                            )
+                            .clip(shape = CircleShape)
+                    )
+
+                }
+            }
+
             Box(
-                modifier = Modifier.height(100.dp).padding(5.dp)
-                    .width(300.dp).background(color = Color.Red)
+                modifier = Modifier
+                    .height(100.dp)
+                    .padding(5.dp)
+                    .width(300.dp)
+                    .background(color = Color.Red)
             )
             Box(
-                modifier = Modifier.height(100.dp).padding(5.dp).
-                width(300.dp).background(color = Color.Green)
+                modifier = Modifier
+                    .height(100.dp)
+                    .padding(5.dp)
+                    .width(300.dp)
+                    .background(color = Color.Green)
             )
             Box(
-                modifier = Modifier.height(100.dp).padding(5.dp).
-                width(300.dp).background(color = Color.Gray)
+                modifier = Modifier
+                    .height(100.dp)
+                    .padding(5.dp)
+                    .width(300.dp)
+                    .background(color = Color.Gray)
             )
             Box(
-                modifier = Modifier.height(100.dp).padding(5.dp).
-                width(300.dp).background(color = Color.Magenta)
+                modifier = Modifier
+                    .height(100.dp)
+                    .padding(5.dp)
+                    .width(300.dp)
+                    .background(color = Color.Magenta)
             )
 
             Box(
-                modifier = Modifier.height(100.dp).padding(5.dp)
-                    .width(300.dp).background(color = Color.Red)
+                modifier = Modifier
+                    .height(100.dp)
+                    .padding(5.dp)
+                    .width(300.dp)
+                    .background(color = Color.Red)
             )
             Box(
-                modifier = Modifier.height(100.dp).padding(5.dp).
-                width(300.dp).background(color = Color.Green)
+                modifier = Modifier
+                    .height(100.dp)
+                    .padding(5.dp)
+                    .width(300.dp)
+                    .background(color = Color.Green)
             )
             Box(
-                modifier = Modifier.height(100.dp).padding(5.dp).
-                width(300.dp).background(color = Color.Gray)
+                modifier = Modifier
+                    .height(100.dp)
+                    .padding(5.dp)
+                    .width(300.dp)
+                    .background(color = Color.Gray)
             )
             Box(
-                modifier = Modifier.height(100.dp).padding(5.dp).
-                width(300.dp).background(color = Color.Magenta)
+                modifier = Modifier
+                    .height(100.dp)
+                    .padding(5.dp)
+                    .width(300.dp)
+                    .background(color = Color.Magenta)
             )
 
             Box(
-                modifier = Modifier.height(100.dp).padding(5.dp)
-                    .width(300.dp).background(color = Color.Red)
+                modifier = Modifier
+                    .height(100.dp)
+                    .padding(5.dp)
+                    .width(300.dp)
+                    .background(color = Color.Red)
             )
             Box(
-                modifier = Modifier.height(100.dp).padding(5.dp).
-                width(300.dp).background(color = Color.Green)
+                modifier = Modifier
+                    .height(100.dp)
+                    .padding(5.dp)
+                    .width(300.dp)
+                    .background(color = Color.Green)
             )
             Box(
-                modifier = Modifier.height(100.dp).padding(5.dp).
-                width(300.dp).background(color = Color.Gray)
+                modifier = Modifier
+                    .height(100.dp)
+                    .padding(5.dp)
+                    .width(300.dp)
+                    .background(color = Color.Gray)
             )
             Box(
-                modifier = Modifier.height(100.dp).padding(5.dp).
-                width(300.dp).background(color = Color.Magenta)
+                modifier = Modifier
+                    .height(100.dp)
+                    .padding(5.dp)
+                    .width(300.dp)
+                    .background(color = Color.Magenta)
             )
             Box(
-                modifier = Modifier.height(100.dp).padding(5.dp)
-                    .width(300.dp).background(color = Color.Red)
+                modifier = Modifier
+                    .height(100.dp)
+                    .padding(5.dp)
+                    .width(300.dp)
+                    .background(color = Color.Red)
             )
             Box(
-                modifier = Modifier.height(100.dp).padding(5.dp).
-                width(300.dp).background(color = Color.Green)
+                modifier = Modifier
+                    .height(100.dp)
+                    .padding(5.dp)
+                    .width(300.dp)
+                    .background(color = Color.Green)
             )
             Box(
-                modifier = Modifier.height(100.dp).padding(5.dp).
-                width(300.dp).background(color = Color.Gray)
+                modifier = Modifier
+                    .height(100.dp)
+                    .padding(5.dp)
+                    .width(300.dp)
+                    .background(color = Color.Gray)
             )
             Box(
-                modifier = Modifier.height(100.dp).padding(5.dp).
-                width(300.dp).background(color = Color.Magenta)
+                modifier = Modifier
+                    .height(100.dp)
+                    .padding(5.dp)
+                    .width(300.dp)
+                    .background(color = Color.Magenta)
             )
 
         }
